5d3c4b1396405faab8c0024f80b00d2623059c4b
akansha204 <akansharoy2008@gmail.com>
2025-09-14 00:05:18 +0530

    done

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,15 +1,63 @@
 "use client";
 import Navbar from '@/components/Navbar'
+import { InfiniteMovingCardsDemo } from '@/components/trendingCards';
+import { Button } from '@/components/ui/button';
+import { HoverBorderGradient } from '@/components/ui/hover-border-gradient';
 import React from 'react'
-import { LoginButton } from '@/components/LoginBtn';
-import { signOut, useSession } from 'next-auth/react';
-import { SignUpButton } from '@/components/SignUpBtn';
 
 export default function page() {
 
     return (
         <>
+            {/* Orchid Depths */}
 
+            {/* Your Content/Components */}
+            <div className="relative z-10 mx-auto ">
+                <div className='flex flex-col mx-auto justify-center mt-10 px-4 sm:px-6 lg:px-8 '>
+
+                    <h1 className='text-4xl sm:text-5xl md:text-6xl text-gray-300 font-medium text-center'>
+                        Unlock the World of <span className='block'>Visual Prompts</span>
+                    </h1>
+                    <p className='text-base sm:text-lg text-gray-500 font-medium pt-6 sm:pt-8 text-center px-4'>
+                        Discover, Filter, and fall in love with the prompts that speak your vibe.
+                    </p>
+                </div>
+
+                <div className='flex flex-wrap justify-center items-center mt-8 gap-4 px-4 sm:px-6 lg:px-8 max-w-7xl mx-auto'>
+                    <HoverBorderGradient className="text-sm sm:text-base cursor-pointer" >
+                        <span className='text-white'>Mood</span>
+                    </HoverBorderGradient>
+                    <HoverBorderGradient className="text-sm sm:text-base cursor-pointer" >
+                        <span className='text-white'>Composition</span>
+                    </HoverBorderGradient>
+                    <HoverBorderGradient className="text-sm sm:text-base cursor-pointer" >
+                        <span className='text-white'>Color Vibe</span>
+                    </HoverBorderGradient>
+                    <HoverBorderGradient className="text-sm sm:text-base cursor-pointer" >
+                        <span className='text-white'>Popularity</span>
+                    </HoverBorderGradient>
+                    <HoverBorderGradient className="text-sm sm:text-base cursor-pointer" >
+                        <span className='text-white'>Fresh Drops</span>
+                    </HoverBorderGradient>
+                </div>
+                <InfiniteMovingCardsDemo />
+
+            </div>
+
+
+
+            {/* <div className='flex flex-col min-h-screen'>
+                <Navbar />
+                <div className='flex flex-col mx-auto justify-center mt-10 px-4 sm:px-6 lg:px-8 '>
+                    <h1 className='text-4xl sm:text-5xl md:text-6xl text-white font-bold text-center'>
+                        Unlock the World of <span className='block'>Visual Prompts</span>
+                    </h1>
+                    <p className='text-base sm:text-lg text-white pt-6 sm:pt-8 text-center px-4'>
+                        Discover, Filter, and fall in love with the prompts that speak your vibe.
+                    </p>
+                </div>
+
+            </div> */}
         </>
 
     )
